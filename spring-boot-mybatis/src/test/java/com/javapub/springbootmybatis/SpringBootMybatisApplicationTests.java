ee8a1072eb7d67a6b3e64a19e8b59fbf58c1640e
wangshiyu <wangshiyu@shuidi-inc.com>
2021-06-29 09:32:11 +0800

    springboot mybatis updateBach

---
+++ spring-boot-mybatis/src/test/java/com/javapub/springbootmybatis/SpringBootMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapub.springbootmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
