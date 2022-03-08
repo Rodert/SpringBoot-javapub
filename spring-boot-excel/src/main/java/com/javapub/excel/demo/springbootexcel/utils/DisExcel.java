368e41413ea43ad5957b8808953d0958576565d3
wsy <wangshiyu@ai-finance.cn>
2022-03-08 19:58:09 +0800

    excel

--- spring-boot-excel/src/main/java/com/javapub/excel/demo/springbootexcel/utils/DisExcel.java
+++ spring-boot-excel/src/main/java/com/javapub/excel/demo/springbootexcel/utils/DisExcel.java
@@ -1,6 +1,7 @@
 package com.javapub.excel.demo.springbootexcel.utils;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -9,15 +10,43 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
- * workbook > sheet > row > cell
+ * workbook > sheet页 > row行 > cell列
  */
 public class DisExcel {
+    public static void main(String[] args) throws Exception {
+        String fileName = "E:\\javapubsworkpace\\SpringBoot-javapub\\spring-boot-excel\\src\\main\\resources\\test.xlsx";
+        DisExcel disExcel = new DisExcel();
+        Map<String, List<Row>> rowsMap = disExcel.getRows(fileName);
+        for (String s : rowsMap.keySet()) {
+            List<Row> rows = rowsMap.get(s);
+            System.out.println("sheetName：" + s + " 包含行数：" + rows.size());
+        }
+    }
+
+    Map<String, List<Row>> getRows(String fileName) throws Exception {
+        List<Sheet> sheets = getSheets(fileName);
+        Map<String, List<Row>> sheetNameAndRowsMap = new HashMap<>(sheets.size());
+        for (Sheet sheet : sheets) {
+            String sheetName = sheet.getSheetName();
+            int physicalNumberOfRows = sheet.getPhysicalNumberOfRows(); // sheet总行数
+            List<Row> rows = new ArrayList<>(physicalNumberOfRows);
+            for (int i = 0; i < physicalNumberOfRows; i++) {
+                rows.add(sheet.getRow(i));
+            }
+            sheetNameAndRowsMap.put(sheetName, rows);
+        }
+        return sheetNameAndRowsMap;
+    }
 
     /**
+     * 获取 sheet
+     *
      * @param fileName
      * @return
      */
