f8825906eb338f15cb80bbffcbb0598714b2651c
wsy <wangshiyu@ai-finance.cn>
2022-03-17 18:17:04 +0800

    es trace

---
+++ spring-boot-elasticsearch/src/main/java/com/javapub/demo/elasticsearch/springbootelasticsearch/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.javapub.demo.elasticsearch.springbootelasticsearch.controller;
+
+public class DemoController {
+
+    /**
+     * 获取文档
+     * 删除文档
+     * 更新文档
+     * 批量
+     */
+
+}
